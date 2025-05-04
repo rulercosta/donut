daebb0e59d1a06b9bcd07a95dd71f4ea7dbf594b
rulercosta <rulercosta.mail@gmail.com>
2025-05-04 16:41:40 +0530

    Refactor donut animation code for improved readability and structure by extracting logic into helper functions

--- donut.go
+++ donut.go
@@ -10,31 +10,31 @@ import (
 
 const (
 	// Screen and buffer dimensions
-	screenWidth 		= 80
-	screenHeight		= 22
-	bufferSize  		= screenWidth * screenHeight
+	screenWidth 		= 		80
+	screenHeight		= 		22
+	bufferSize  		= 		screenWidth * screenHeight
 
 	// Donut geometry and animation
-	phiStep            	= 0.07
-	thetaStep          	= 0.02
-	frameDelay         	= 30 * time.Millisecond
-	radiusMajor        	= 2.0
-	radiusMinor        	= 1.0
-	distanceOffset     	= 5.0
-	xScale             	= 30.0
-	yScale             	= 15.0
-	rotationAIncrement 	= 0.04
-	rotationBIncrement 	= 0.02
+	phiStep           	= 		0.07
+	thetaStep         	= 		0.02
+	frameDelay        	= 		30 * time.Millisecond
+	radiusMajor       	= 		2.0
+	radiusMinor       	= 		1.0
+	distanceOffset    	= 		5.0
+	xScale            	= 		30.0
+	yScale            	= 		15.0
+	rotationAIncrement	= 		0.04
+	rotationBIncrement	= 		0.02
 
 	// Luminance
-	luminanceLevels 	= 8
-	luminanceChars  	= ".,-~:;=!*#$@"
-
+	luminanceLevels 	=		8
+	luminanceChars  	=		".,-~:;=!*#$@"
+	
 	// Terminal escape sequences
-	hideCursor  		= "\x1b[?25l"
-	showCursor  		= "\x1b[?25h"
-	clearScreen 		= "\x1b[2J"
-	homeCursor  		= "\x1b[H"
+	hideCursor 			=		"\x1b[?25l"
+	showCursor 			=		"\x1b[?25h"
+	clearScreen			=		"\x1b[2J"
+	homeCursor 			=		"\x1b[H"
 )
 
 func main() {
@@ -79,32 +79,54 @@ func computeFrame(outputBuffer []byte, zBuffer []float64, rotationA, rotationB f
 }
 
 func project3DTo2D(angleTheta, anglePhi, rotationA, rotationB float64) (int, int, int, float64) {
-	sinTheta := math.Sin(angleTheta)
-	cosPhi := math.Cos(anglePhi)
-	sinRotationA := math.Sin(rotationA)
-	sinPhi := math.Sin(anglePhi)
-	cosRotationA := math.Cos(rotationA)
-	h := cosPhi + radiusMajor
-	invDepth := 1 / (sinTheta*h*sinRotationA + sinPhi*cosRotationA + distanceOffset)
-	cosTheta := math.Cos(angleTheta)
-	cosRotationB := math.Cos(rotationB)
-	sinRotationB := math.Sin(rotationB)
-	temp := sinTheta*h*cosRotationA - sinPhi*sinRotationA
-	x := int(float64(screenWidth/2) + xScale*invDepth*(cosTheta*h*cosRotationB-temp*sinRotationB))
-	y := int(float64(screenHeight/2) + yScale*invDepth*(cosTheta*h*sinRotationB+temp*cosRotationB))
+	sinTheta, cosTheta := getSinCos(angleTheta)
+	sinPhi, cosPhi := getSinCos(anglePhi)
+	sinRotationA, cosRotationA := getSinCos(rotationA)
+	sinRotationB, cosRotationB := getSinCos(rotationB)
+	h := getMajorRadiusOffset(cosPhi)
+	invDepth := getInverseDepth(sinTheta, h, sinRotationA, sinPhi, cosRotationA)
+	temp := getTemp(sinTheta, h, cosRotationA, sinPhi, sinRotationA)
+	x := getScreenX(cosTheta, h, cosRotationB, temp, sinRotationB, invDepth)
+	y := getScreenY(cosTheta, h, sinRotationB, temp, cosRotationB, invDepth)
 	bufferOffset := x + screenWidth*y
 	return x, y, bufferOffset, invDepth
 }
 
 func calculateLuminanceIndex(angleTheta, anglePhi, rotationA, rotationB float64) int {
-	sinTheta := math.Sin(angleTheta)
-	cosPhi := math.Cos(anglePhi)
-	sinRotationA := math.Sin(rotationA)
-	sinPhi := math.Sin(anglePhi)
-	cosRotationA := math.Cos(rotationA)
-	cosRotationB := math.Cos(rotationB)
-	luminance := (sinPhi*sinRotationA-sinTheta*cosPhi*cosRotationA)*cosRotationB - sinTheta*cosPhi*sinRotationA - sinPhi*cosRotationA - math.Cos(angleTheta)*cosPhi*math.Sin(rotationB)
+	sinTheta, cosTheta := getSinCos(angleTheta)
+	sinPhi, cosPhi := getSinCos(anglePhi)
+	sinRotationA, cosRotationA := getSinCos(rotationA)
+	sinRotationB, cosRotationB := getSinCos(rotationB)
+	luminance := getLuminance(sinTheta, cosTheta, sinPhi, cosPhi, sinRotationA, cosRotationA, sinRotationB, cosRotationB)
 	return int(float64(luminanceLevels) * luminance)
+}
+
+func getSinCos(angle float64) (float64, float64) {
+	return math.Sin(angle), math.Cos(angle)
+}
+
+func getMajorRadiusOffset(cosPhi float64) float64 {
+	return cosPhi + radiusMajor
+}
+
+func getInverseDepth(sinTheta, h, sinRotationA, sinPhi, cosRotationA float64) float64 {
+	return 1 / (sinTheta*h*sinRotationA + sinPhi*cosRotationA + distanceOffset)
+}
+
+func getTemp(sinTheta, h, cosRotationA, sinPhi, sinRotationA float64) float64 {
+	return sinTheta*h*cosRotationA - sinPhi*sinRotationA
+}
+
+func getScreenX(cosTheta, h, cosRotationB, temp, sinRotationB, invDepth float64) int {
+	return int(float64(screenWidth/2) + xScale*invDepth*(cosTheta*h*cosRotationB-temp*sinRotationB))
+}
+
+func getScreenY(cosTheta, h, sinRotationB, temp, cosRotationB, invDepth float64) int {
+	return int(float64(screenHeight/2) + yScale*invDepth*(cosTheta*h*sinRotationB+temp*cosRotationB))
+}
+
+func getLuminance(sinTheta, cosTheta, sinPhi, cosPhi, sinRotationA, cosRotationA, sinRotationB, cosRotationB float64) float64 {
+	return (sinPhi*sinRotationA-sinTheta*cosPhi*cosRotationA)*cosRotationB - sinTheta*cosPhi*sinRotationA - sinPhi*cosRotationA - cosTheta*cosPhi*sinRotationB
 }
 
 func getLuminanceChar(luminanceIndex int) byte {
